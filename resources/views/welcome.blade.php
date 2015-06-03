8f496594a16ff390b680315f9e92be97345227c5
Peter <peter.argue@gmail.com>
2015-06-03 16:49:54 -0700

    Update landing page text to guide people to login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <h1>TMLP Stats</h1>
-<p>Welcome to the Home of TMLP Stats! We're still building an awesome new stats site, but it's not quite ready yet. Check back soon!</p>
+<p>Welcome to the Home of TMLP Stats! Click Login/Register in the top right to login and get started.</p>
 @endsection
