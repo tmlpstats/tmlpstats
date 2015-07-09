0ca3469188dff987e916f7947ad0fee8c8ba2dcf
Peter <peter.argue@gmail.com>
2015-07-09 09:44:05 -0700

    Add Authenticated requirements to Admin Dashboard controller

--- app/Http/Controllers/GlobalReportController.php
+++ app/Http/Controllers/GlobalReportController.php
@@ -14,6 +14,17 @@ use Input;
 
 class GlobalReportController extends Controller {
 
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
