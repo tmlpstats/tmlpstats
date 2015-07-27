d29310c8bc20619f3cd0836260c75562b2f3a6be
Peter <peter.argue@gmail.com>
2015-07-26 21:32:47 -0700

    Fix smtp configuration

--- config/mail.php
+++ config/mail.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.launchpointinternational.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'tmlpstats@launchpointinternational.com', 'name' => 'TMLP Stats'],
 
     /*
     |--------------------------------------------------------------------------
