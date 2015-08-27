ec5903d2b53ef8b9e09d186d273dacc05673b259
Peter <peter.argue@gmail.com>
2015-08-26 23:20:10 -0700

    Add Accountability Roster tab to valid sheets list

--- app/Import/Xlsx/ImportDocument/ImportDocumentAbstract.php
+++ app/Import/Xlsx/ImportDocument/ImportDocumentAbstract.php
@@ -133,6 +133,7 @@ abstract class ImportDocumentAbstract extends \TmlpStats\Import\ImportDocument
     protected function isNorthAmericaSheet($inputWorksheetNames)
     {
         $expectedSheets = $this->naSheetNames;
+        array_push($expectedSheets, 'Accountability Roster');
         array_push($expectedSheets, 'Instructions - Revision History');
 
         return $this->hasExpectedSheets($inputWorksheetNames, $expectedSheets);
