56cf8a26cb33d6bab3c1712b1f3dbf2a1ce77d0f
Peter <peter.argue@gmail.com>
2015-10-15 22:16:38 -0700

    Fix Nav bar name display

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -37,7 +37,7 @@
               <li {!! Request::is('/') ? 'class="active"' : '' !!}><a href="{{ url('/auth/login') }}">Login/Register</a></li>
             @else
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->first_name }} <span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->firstName }} <span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
                   <!--<li><a href="{{ url('/user/profile') }}">Profile</a></li>-->
                   <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
