473522bedc4c7e9435901e0a0cc93c1f021daea3
Peter <peter.argue@gmail.com>
2015-10-26 19:12:25 -0700

    Add arragnement for courses, and update view

---
+++ src/app/Reports/Arrangements/CoursesWithEffectiveness.php
@@ -0,0 +1,57 @@
+<?php namespace TmlpStats\Reports\Arrangements;
+
+class CoursesWithEffectiveness extends BaseArrangement
+{
+
+    /* Builds an array of courses with effectiveness calculations
+     *
+     */
+    public function build($data)
+    {
+        $courses = $data['courses'];
+        $statsReport = $data['statsReport'];
+
+        $reportData = [];
+        foreach ($courses as $courseData) {
+
+            $type = $courseData->course->type;
+
+            $course = [
+                'location'  => $courseData->course->location ?: $courseData->course->center->name,
+                'startDate' => $courseData->course->startDate,
+                'type'      => $type,
+            ];
+
+            $copyFields = [
+                'quarterStartTer',
+                'quarterStartStandardStarts',
+                'quarterStartXfer',
+                'currentTer',
+                'currentStandardStarts',
+                'currentXfer',
+                'completedStandardStarts',
+                'potentials',
+                'registrations',
+            ];
+
+            foreach ($copyFields as $field) {
+                $course[$field] = $courseData->$field;
+            }
+
+            if ($course['startDate']->lt($statsReport->reportingDate)) {
+                $course['completionStats']['registrationFulfillment'] = $courseData->currentTer
+                    ? round(($courseData->currentStandardStarts / $courseData->currentTer) * 100)
+                    : 0;
+                $course['completionStats']['registrationEffectiveness'] = $courseData->potentials
+                    ? round(($courseData->registrations / $courseData->potentials) * 100)
+                    : 0;
+
+                $type = 'completed';
+            }
+
+            $reportData[$type][] = $course;
+        }
+
+        return compact('reportData');
+    }
+}
--- src/resources/views/statsreports/details/courses.blade.php
+++ src/resources/views/statsreports/details/courses.blade.php
@@ -1,46 +1,100 @@
 <div class="table-responsive">
-    @foreach ($courses as $type => $group)
-        <?php
-            if (!in_array($type, ['CAP', 'CPC'])) {
-                continue;
-            }
-        ?>
+    @foreach (['CAP', 'CPC'] as $type)
+        @if (isset($reportData[$type]))
+            <table class="table table-condensed table-striped table-hover">
+                <thead>
+                <tr>
+                    <th colspan="12">{{ ucwords($type) }}</th>
+                </tr>
+                <tr>
+                    <th>&nbsp;</th>
+                    <th style="border-right: 2px solid #DDD;">&nbsp;</th>
+                    <th colspan="3" style="border-right: 2px solid #DDD; text-align: center">Quarter Starting</th>
+                    <th colspan="3" style="text-align: center">Current</th>
+                </tr>
+                <tr>
+                    <th>Location</th>
+                    <th style="border-right: 2px solid #DDD;">Date</th>
+                    <th style="text-align: center">Total Ever Registered</th>
+                    <th style="text-align: center">Standard Starts</th>
+                    <th style="border-right: 2px solid #DDD; text-align: center">Transferred in from Previous</th>
+                    <th style="text-align: center">Total Ever Registered</th>
+                    <th style="text-align: center">Standard Starts</th>
+                    <th style="text-align: center">Transferred in from Previous</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($reportData[$type] as $courseData)
+                    <tr>
+                        <td>{{ $courseData['location'] }}</td>
+                        <td style="border-right: 2px solid #DDD;">{{ $courseData['startDate']->format("M j, Y") }}</td>
+                        <td style="text-align: center">{{ $courseData['quarterStartTer'] }}</td>
+                        <td style="text-align: center">{{ $courseData['quarterStartStandardStarts'] }}</td>
+                        <td style="border-right: 2px solid #DDD; text-align: center">{{ $courseData['quarterStartXfer'] }}</td>
+                        <td style="text-align: center">{{ $courseData['currentTer'] }}</td>
+                        <td style="text-align: center">{{ $courseData['currentStandardStarts'] }}</td>
+                        <td style="text-align: center">{{ $courseData['currentXfer'] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
+    @endforeach
+
+    @if (isset($reportData['completed']))
+        <br/>
+        <h3>Completed</h3>
         <table class="table table-condensed table-striped table-hover">
             <thead>
             <tr>
-                <th colspan="12">{{ ucwords($type) }}</th>
+                <th style="border-top: 2px solid #DDD;">&nbsp;</th>
+                <th style="border-top: 2px solid #DDD;">&nbsp;</th>
+                <th style="border-top: 2px solid #DDD; border-right: 2px solid #DDD;">&nbsp;</th>
+                <th colspan="3" style="border-top: 2px solid #DDD; border-right: 2px solid #DDD; text-align: center">
+                    Quarter Starting
+                </th>
+                <th colspan="3" style="border-top: 2px solid #DDD; border-right: 2px solid #DDD; text-align: center">
+                    Current
+                </th>
+                <th colspan="5" style="border-top: 2px solid #DDD; text-align: center">Completion</th>
             </tr>
             <tr>
+                <th>&nbsp;</th>
                 <th>Location</th>
                 <th style="border-right: 2px solid #DDD;">Date</th>
-                <th>Quarter Start TER</th>
-                <th>Quarter Start SS</th>
-                <th style="border-right: 2px solid #DDD;">Quarter Start Xfer</th>
-                <th>Current TER</th>
-                <th>Current SS</th>
-                <th style="border-right: 2px solid #DDD;">Current Xfer</th>
-                <th>Completed SS</th>
-                <th>Potentials</th>
-                <th>Registrations</th>
+                <th style="text-align: center">TER</th>
+                <th style="text-align: center">Standard Starts</th>
+                <th style="border-right: 2px solid #DDD; text-align: center">Xfer</th>
+                <th style="text-align: center">TER</th>
+                <th style="text-align: center">Standard Starts</th>
+                <th style="border-right: 2px solid #DDD; text-align: center">Xfer</th>
+                <th style="text-align: center">Standard Starts</th>
+                <th style="text-align: center">Potentials</th>
+                <th style="border-right: 2px solid #DDD; text-align: center">Registrations</th>
+                <th style="text-align: center">Reg Fulfillment</th>
+                <th style="text-align: center">Reg Effectiveness</th>
             </tr>
             </thead>
             <tbody>
-                @foreach($group as $courseData)
-                    <tr>
-                        <td>{{ $courseData->course->location ?: $courseData->course->center->name }}</td>
-                        <td style="border-right: 2px solid #DDD;">{{ $courseData->course->startDate->format("M j, Y") }}</td>
-                        <td style="text-align: center">{{ $courseData->quarterStartTer }}</td>
-                        <td style="text-align: center">{{ $courseData->quarterStartStandardStarts }}</td>
-                        <td style="border-right: 2px solid #DDD; text-align: center">{{ $courseData->quarterStartXfer }}</td>
-                        <td style="text-align: center">{{ $courseData->currentTer }}</td>
-                        <td style="text-align: center">{{ $courseData->currentStandardStarts }}</td>
-                        <td style="border-right: 2px solid #DDD; text-align: center">{{ $courseData->currentXfer }}</td>
-                        <td style="text-align: center">{{ $courseData->completedStandardStarts }}</td>
-                        <td style="text-align: center">{{ $courseData->potentials }}</td>
-                        <td style="text-align: center">{{ $courseData->registrations }}</td>
-                    </tr>
-                @endforeach
+            @foreach ($reportData['completed'] as $courseData)
+                <tr>
+                    <td>{{ $courseData['type'] }}</td>
+                    <td>{{ $courseData['location'] }}</td>
+                    <td style="border-right: 2px solid #DDD;">{{ $courseData['startDate']->format("M j, Y") }}</td>
+                    <td style="text-align: center">{{ $courseData['quarterStartTer'] }}</td>
+                    <td style="text-align: center">{{ $courseData['quarterStartStandardStarts'] }}</td>
+                    <td style="border-right: 2px solid #DDD; text-align: center">{{ $courseData['quarterStartXfer'] }}</td>
+                    <td style="text-align: center">{{ $courseData['currentTer'] }}</td>
+                    <td style="text-align: center">{{ $courseData['currentStandardStarts'] }}</td>
+                    <td style="border-right: 2px solid #DDD; text-align: center">{{ $courseData['currentXfer'] }}</td>
+                    <td style="text-align: center">{{ $courseData['completedStandardStarts'] }}</td>
+                    <td style="text-align: center">{{ $courseData['potentials'] }}</td>
+                    <td style="border-right: 2px solid #DDD; text-align: center">{{ $courseData['registrations'] }}</td>
+                    <td style="text-align: center">{{ $courseData['completionStats']['registrationFulfillment'] }}%</td>
+                    <td style="text-align: center">{{ $courseData['completionStats']['registrationEffectiveness'] }}%</td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
-    @endforeach
+    @endif
 </div>
