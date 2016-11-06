5da57a40f8f506f598e9d6bf64fea16b29b7ca9f
Peter <peter.argue@gmail.com>
2016-11-06 12:13:18 -0800

    Add next and back buttons on center report page

--- src/resources/views/statsreports/show.blade.php
+++ src/resources/views/statsreports/show.blade.php
@@ -13,7 +13,21 @@ $nextQtrAccountabilities = $centerReportingDate->canShowNextQtrAccountabilities(
         @endif
     </h2>
     @can ('index', TmlpStats\StatsReport::class)
-    <a href="{{ url('/home') }}"><< See All</a><br/><br/>
+    <div class="row">
+        <div class="col-sm-10">
+            <a href="{{ url('/home') }}"><< See All</a><br/><br/>
+        </div>
+        <div class="col-sm-1 hide-mobile" style="text-align: right">
+            @if (isset($lastReport))
+                <a href="{{ url('/statsreports/' . $lastReport->id) }}"><< Back</a><br/><br/>
+            @endif
+        </div>
+        <div class="col-sm-1 hide-mobile">
+            @if (isset($nextReport))
+                <a href="{{ url('/statsreports/' . $nextReport->id) }}">Next >></a><br/><br/>
+            @endif
+        </div>
+    </div>
     @endcan
     @if ($reportToken)
         <div class="modal fade" id="reportLinkModel" tabindex="-1" role="dialog" aria-labelledby="reportLinkModelLabel">
