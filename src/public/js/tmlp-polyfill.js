94d0b110f735ad89332d0e6091bbf970cca023e5
James Crasta <jcrasta@underarmour.com>
2017-03-03 01:04:59 -0700

    Polyfill String.prototype.startsWith with the simplest variant

--- src/public/js/tmlp-polyfill.js
+++ src/public/js/tmlp-polyfill.js
@@ -7,3 +7,10 @@ if (!window.console.log) {
         // does nothing
     }
 }
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position) {
+        position = position || 0
+        return this.substr(position, searchString.length) === searchString
+    }
+}
