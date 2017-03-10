04d79afde1ec30e9fd3ca25b4a705d9d30aa277c
Peter <peter.argue@gmail.com>
2017-03-09 20:11:21 -0800

    Add current URL to feedback email

--- src/resources/assets/js/classic/tmlpstats.js
+++ src/resources/assets/js/classic/tmlpstats.js
@@ -95,6 +95,8 @@ window.Tmlp = (function(window, $) {
     function enableFeedback(config) {
         var feedbackFormDirty = false;
 
+        $('#feedbackUrl').val(location.href);
+
         function resetFeedbackForm() {
 
             $("#feedbackSubmitResult").hide();
@@ -116,77 +118,78 @@ window.Tmlp = (function(window, $) {
 
         resetFeedbackForm();
 
-            $("#contactLink").on('click', function() {
+        $("#contactLink").on('click', function() {
 
-                if (feedbackFormDirty) {
-                    resetFeedbackForm();
-                }
+            if (feedbackFormDirty) {
+                resetFeedbackForm();
+            }
 
-                $('#feedbackModel').modal('show');
-            });
+            $('#feedbackModel').modal('show');
+        });
 
-            $("#submitFeedback").on('click', function() {
+        $("#submitFeedback").on('click', function() {
 
-                $("#submitFeedback").attr("disabled", true);
+            $("#submitFeedback").attr("disabled", true);
 
-                var data = {};
-                data.dataType = 'JSON';
-                data.name = $("input[name=name]").val();
-                data.email = $("input[name=email]").val();
-                data.message = $("textarea[name=message]").val();
+            var data = {};
+            data.dataType = 'JSON';
+            data.name = $("input[name=name]").val();
+            data.email = $("input[name=email]").val();
+            data.message = $("textarea[name=message]").val();
+            data.feedbackUrl = $("input[name=feedbackUrl]").val();
 
-                var copySender = $("input[name=copySender]").val();
-                if (copySender) {
-                    data.copySender = copySender;
-                }
+            var copySender = $("input[name=copySender]").val();
+            if (copySender) {
+                data.copySender = copySender;
+            }
 
-                feedbackFormDirty = true;
+            feedbackFormDirty = true;
 
-                $.ajax({
-                    type: "POST",
-                    url: config.feedbackUrl,
-                    data: $.param(data),
-                    beforeSend: function (request) {
-                        request.setRequestHeader("X-CSRF-TOKEN", config.csrfToken);
-                    },
-                    success: function(response) {
-                        var $resultDiv = $("#feedbackSubmitResult");
-                        $resultDiv.find("span.message").html(response.message);
-                        if (response.success) {
-                            $resultDiv.removeClass("alert-danger");
-                            $resultDiv.addClass("alert-success");
-                        } else {
-                            $resultDiv.removeClass("alert-success");
-                            $resultDiv.addClass("alert-danger");
-                        }
-                        $resultDiv.show();
-
-                        $("#feedbackForm").hide();
-                        $("#submitFeedback").hide();
-                        $("#submitFeedbackCancel").html('Close');
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        var code = jqXHR.status;
-
-                        var message = '';
-                        if (code == 404) {
-                            message = 'We were unable to find that report. Please try validating and submitting your report again.';
-                        } else if (code == 403) {
-                            message = 'You are not allowed to submit this report.';
-                        } else {
-                            message = 'There was a problem submitting your report. Please try again.';
-                        }
-
-                        var $resultDiv = $("#feedbackSubmitResult");
-                        $resultDiv.find("span.message").html('<p>' + message + '</p>');
+            $.ajax({
+                type: "POST",
+                url: config.feedbackUrl,
+                data: $.param(data),
+                beforeSend: function (request) {
+                    request.setRequestHeader("X-CSRF-TOKEN", config.csrfToken);
+                },
+                success: function(response) {
+                    var $resultDiv = $("#feedbackSubmitResult");
+                    $resultDiv.find("span.message").html(response.message);
+                    if (response.success) {
+                        $resultDiv.removeClass("alert-danger");
+                        $resultDiv.addClass("alert-success");
+                    } else {
                         $resultDiv.removeClass("alert-success");
                         $resultDiv.addClass("alert-danger");
-                        $resultDiv.show();
-
-                        $("#submitFeedback").attr("disabled", false);
                     }
-                });
+                    $resultDiv.show();
+
+                    $("#feedbackForm").hide();
+                    $("#submitFeedback").hide();
+                    $("#submitFeedbackCancel").html('Close');
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    var code = jqXHR.status;
+
+                    var message = '';
+                    if (code == 404) {
+                        message = 'We were unable to find that report. Please try validating and submitting your report again.';
+                    } else if (code == 403) {
+                        message = 'You are not allowed to submit this report.';
+                    } else {
+                        message = 'There was a problem submitting your report. Please try again.';
+                    }
+
+                    var $resultDiv = $("#feedbackSubmitResult");
+                    $resultDiv.find("span.message").html('<p>' + message + '</p>');
+                    $resultDiv.removeClass("alert-success");
+                    $resultDiv.addClass("alert-danger");
+                    $resultDiv.show();
+
+                    $("#submitFeedback").attr("disabled", false);
+                }
             });
+        });
     }
 
     //////////////////////////////
---
+++ src/resources/views/partials/feedback.blade.php
@@ -0,0 +1,47 @@
+<div class="hs">
+    <button id="contactLink" href="#" title="Feedback">
+        <div id="feedbackTabText">Feedback</div>
+    </button>
+</div>
+
+<div class="modal fade" id="feedbackModel" tabindex="-1" role="dialog" aria-labelledby="feedbackModelLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="feedbackModelLabel">Send us your feedback</h4>
+            </div>
+            <div class="modal-body">
+                We would love to hear your feedback! If you have any comments, ideas, suggestions, issues, or anything else, please let us know below.
+                <br/><br/>
+                Also, if you or a TMLP member/graduate you know are interested in participating in creating a new future of statistics for TMLP, let us know that too!
+                <br/><br/>
+                <div id="feedbackSubmitResult" class="alert" role="alert" style="display:none">
+                    <span class="message"></span>
+                </div>
+                <div id="feedbackForm">
+                    {!! Form::open(['url' => url('/feedback')]) !!}
+                    {!! Form::hidden('feedbackUrl', 'not captured', ['id' => 'feedbackUrl']) !!}
+
+                    <br/>
+                    {!! Form::label('name', 'Name:', ['class' => 'control-label']) !!}
+                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                    <br/>
+                    {!! Form::label('email', 'Email:', ['class' => 'control-label']) !!}
+                    {!! Form::email('email', null, ['class' => 'form-control']) !!}
+                    <br/>
+                    {!! Form::label('message', 'Message:', ['class' => 'control-label']) !!}
+                    {!! Form::textarea('message', null, ['class' => 'form-control', 'rows' => '10']) !!}
+                    <br/>
+                    {!! Form::checkbox('copySender', 1, true, []) !!} Send me a copy
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal" id="submitFeedbackCancel">Cancel</button>
+                <button type="button" class="btn btn-primary" id="submitFeedback">Send</button>
+            </div>
+        </div>
+    </div>
+</div>
