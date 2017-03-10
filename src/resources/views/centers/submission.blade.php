ad327c5aee092e01e6ec1a937614b945242e6dc7
Peter <peter.argue@gmail.com>
2017-03-09 19:18:13 -0800

    remove 'already'

--- src/resources/views/centers/submission.blade.php
+++ src/resources/views/centers/submission.blade.php
@@ -4,7 +4,7 @@
     <h1>
         Team {{ $center->name }} <span class="date-title">- {{ $reportingDate->format('F j, Y') }}</span>
         @if ($alreadySubmitted)
-            <span class="already-submitted">(already submitted)</span>
+            <span class="already-submitted">(submitted)</span>
         @endif
     </h1>
     <div id="react-routed-flow"></div>
