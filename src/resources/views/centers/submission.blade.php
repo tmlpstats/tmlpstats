6e8cd6276dca353d2bbf576825ce9c9df5d1cbc0
Peter <peter.argue@gmail.com>
2017-03-09 19:12:06 -0800

    Add date and indicator if report has already been submitted for date

--- src/resources/views/centers/submission.blade.php
+++ src/resources/views/centers/submission.blade.php
@@ -1,6 +1,11 @@
 @extends('template')
 
 @section('content')
-    <h1>Team {{ $center->name }}</h1>
+    <h1>
+        Team {{ $center->name }} <span class="date-title">- {{ $reportingDate->format('F j, Y') }}</span>
+        @if ($alreadySubmitted)
+            <span class="already-submitted">(already submitted)</span>
+        @endif
+    </h1>
     <div id="react-routed-flow"></div>
 @endsection
