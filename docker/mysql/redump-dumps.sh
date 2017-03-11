b011d814984993663a36829dc216351b622063b1
James Crasta <jcrasta@underarmour.com>
2017-03-11 16:41:32 -0700

    Database names are now tmlpstats_main

--- docker/mysql/redump-dumps.sh
+++ docker/mysql/redump-dumps.sh
@@ -2,6 +2,6 @@
 #
 MYSQLDUMP=mysqldump -u root --password=doesntmatter 
 
-$MYSQLDUMP --no-data tmlpstats_dev > /output/010-structure.sql 
-$MYSQLDUMP --no-create-info tmlpstats_dev \
+$MYSQLDUMP --no-data tmlpstats_main > /output/010-structure.sql 
+$MYSQLDUMP --no-create-info tmlpstats_main \
 	migrations accountabilities withdraw_codes > /output/020-lookups.sql
