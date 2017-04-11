c443a63d6632ef3e6b40fc199d89f0001ed8a163
James Crasta <jcrasta@underarmour.com>
2017-04-11 17:10:19 -0600

    Document storeProxy better

--- src/resources/assets/js/storeProxy.js
+++ src/resources/assets/js/storeProxy.js
@@ -1,22 +1,43 @@
+/**
+ * StoreProxy is needed currently to deal with potential circular import issues.
+ *
+ * If anything is imported by a thing which might be part of the tree involving
+ * the store (reducers, data.js files, etc) then it will have weird reference
+ * errors importing the store.
+ *
+ * Once the store is connected, the `dispatch` and `getState` functions point
+ * to the ones on the real store.
+ */
 class StoreProxy {
     constructor() {
         this.store = null
         this.delayed = []
     }
 
+    /**
+     * Queues actions that might've been dispatched before the store is connected.
+     *
+     * We expect this to be replaced by 'setStore' with the real dispatcher later.
+     */
     dispatch(x) {
         this.delayed.push(x)
     }
 
+    /** Fake getState should be replaced by the real store later. */
     getState() {
         return this.store? this.store.getState() : undefined
     }
 
     setStore(store) {
         this.store = store
-        this.dispatch = store.dispatch
-        this.getState = store.getState
-        this.delayed.forEach(x => store.dispatch(x))
+        // Overwrite dispatch and getState, rebinding them just in case.
+        this.dispatch = store.dispatch.bind(store)
+        this.getState = store.getState.bind(store)
+
+        // Dispatch any previously delayed items.
+        while (this.delayed.length) {
+            store.dispatch(this.delayed.shift())
+        }
     }
 }
 
