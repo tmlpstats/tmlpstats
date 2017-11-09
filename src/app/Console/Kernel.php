d11b2aaebf9887d0c878e991d44342a7da1f84d2
Peter <peter.argue@gmail.com>
2017-11-08 18:49:49 -0800

    Add command to send test emails

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends ConsoleKernel
         Commands\SanitizeDb::class,
         Commands\MergeDuplicatePeople::class,
         Commands\FlushCacheTagCommand::class,
+        Commands\SendTestEmailCommand::class,
     ];
 
     /**
---
+++ src/resources/views/emails/test.blade.php
@@ -0,0 +1,6 @@
+Hi,<br>
+<br>
+This is a test email. Please reply letting us know you got it.
+<br>
+Best,
+The TMLP Stats Team
