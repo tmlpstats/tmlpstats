4f9d3e9e2e2643dd2f539272a0458f209e3d782b
Peter <peter.argue@gmail.com>
2017-11-08 18:56:42 -0800

    Fix email template spacing

--- src/resources/views/emails/test.blade.php
+++ src/resources/views/emails/test.blade.php
@@ -1,6 +1,6 @@
 Hi,<br>
 <br>
-This is a test email. Please reply letting us know you got it.
+This is a test email. Please reply letting us know you got it.<br>
 <br>
-Best,
-The TMLP Stats Team
+Best,<br>
+The TMLP Stats Team<br>
