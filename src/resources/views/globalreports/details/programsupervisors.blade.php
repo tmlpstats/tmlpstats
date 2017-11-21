2232581a2874c938eb3dc06c4c14a6768fca456b
Peter <peter.argue@gmail.com>
2017-11-20 18:23:16 -0800

    Add column to Program Supervisor report with T2 only

--- src/resources/views/globalreports/details/programsupervisors.blade.php
+++ src/resources/views/globalreports/details/programsupervisors.blade.php
@@ -11,7 +11,8 @@
     <th>Currently on Team 2</th>
     <th>Team 2 Incoming</th>
     <th>Team 2 Completing</th>
-    <th>Team 2 Room</th>
+    <th>Team 2 Only</th>
+    <th>Team 2 &amp; PM/CL</th>
     <th>Team 2 Registration Event</th>
 </thead>
 <tbody>
@@ -27,6 +28,7 @@
     <td>{{ $centerData['team2Current'] }}</td>
     <td>{{ $centerData['team2Incoming'] }}</td>
     <td>{{ $centerData['team2Completing'] }}</td>
+    <td>{{ $centerData['team2RoomSatPm'] }}</td>
     <td>{{ $centerData['team2Room'] }}</td>
     <td>{{ $centerData['team2RegistrationEvent'] }}</td>
 </tr>
@@ -42,6 +44,7 @@
     <th>{{ $totals['team2Current'] }}</th>
     <th>{{ $totals['team2Incoming'] }}</th>
     <th>{{ $totals['team2Completing'] }}</th>
+    <th>{{ $totals['team2RoomSatPm'] }}</th>
     <th>{{ $totals['team2Room'] }}</th>
     <th>{{ $totals['team2RegistrationEvent'] }}</th>
 </tr>
