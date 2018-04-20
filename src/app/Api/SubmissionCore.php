218f26df732d8bcb3a0be3d63f1057c9c7f8a190
Peter <pdarg@users.noreply.github.com>
2018-04-20 09:16:53 -0700

    Revert "Revert "Require ProgramLeader travel at the same time as team""

--- src/app/Api/SubmissionCore.php
+++ src/app/Api/SubmissionCore.php
@@ -601,17 +601,23 @@ class SubmissionCore extends AuthenticatedApiBase
     public function calculateProgramLeaderAttending(Models\Center $center, Carbon $reportingDate)
     {
         $leaders = App::make(Api\Submission\ProgramLeader::class)->allForCenter($center, $reportingDate, true);
-        $pmAttending = 0;
-        $clAttending = 0;
+        $pmAttending = null;
+        $clAttending = null;
 
         // This is done due to the limitations of our current storage method.
         // In the future, we should move towards a place we can loop people, allowing situations like multiple classroom leaders (where one's an apprentice or specifically during a weekend of a CL change)
         if (($pmId = $leaders['meta']['programManager']) !== null) {
-            $pmAttending += ($leaders[$pmId]->attendingWeekend) ? 1 : 0;
+            if (!is_null($leaders[$pmId]->attendingWeekend)) {
+                $pmAttending = (int) $pmAttending;
+                $pmAttending += ($leaders[$pmId]->attendingWeekend ? 1 : 0 );
+            }
         }
 
         if (($clId = $leaders['meta']['classroomLeader']) !== null && $clId != $pmId) {
-            $clAttending += ($leaders[$clId]->attendingWeekend) ? 1 : 0;
+            if (!is_null($leaders[$clId]->attendingWeekend)) {
+                $clAttending = (int) $clAttending;
+                $clAttending += ($leaders[$clId]->attendingWeekend ? 1 : 0 );
+            }
         }
 
         return [$pmAttending, $clAttending];
--- src/app/Validate/Objects/ApiProgramLeaderValidator.php
+++ src/app/Validate/Objects/ApiProgramLeaderValidator.php
@@ -4,9 +4,12 @@ namespace TmlpStats\Validate\Objects;
 use App;
 use Respect\Validation\Validator as v;
 use TmlpStats\Api;
+use TmlpStats\Traits;
 
 class ApiProgramLeaderValidator extends ApiObjectsValidatorAbstract
 {
+    use Traits\ValidatesTravelWithConfig;
+
     protected $accountabilityMap = [
         'programManager' => 'Program Manager',
         'classroomLeader' => 'Classroom Leader',
@@ -21,12 +24,15 @@ class ApiProgramLeaderValidator extends ApiObjectsValidatorAbstract
         $this->dataValidators['phone'] = v::phone();
         $this->dataValidators['email'] = v::email();
         $this->dataValidators['accountability'] = v::in(array_keys($this->accountabilityMap));
-        $this->dataValidators['attendingWeekend'] = v::boolType();
+        $this->dataValidators['attendingWeekend'] = v::optional(v::boolType());
     }
 
     protected function validate($data)
     {
         if (!$this->validateEmail($data)) {
+            $this->isValid = false;
+        }
+        if (!$this->validateTravel($data)) {
             $this->isValid = false;
         }
 
@@ -54,5 +60,28 @@ class ApiProgramLeaderValidator extends ApiObjectsValidatorAbstract
         }
 
         return true;
+    }
+
+    public function validateTravel($data)
+    {
+        // attendingWeekend must be reported starting after the configured date
+        if (!$this->isTimeToCheckTravel()) {
+            return true;
+        }
+
+        $isValid = true;
+        if (is_null($data->attendingWeekend)) {
+            $accountability = $this->accountabilityMap[$data->accountability] ?? $data->accountability;
+            $this->addMessage('error', [
+                'id' => 'PROGRAMLEADER_ATTENDING_WEEKEND_MISSING',
+                'ref' => $data->getReference(['field' => 'attendingWeekend']),
+                'params' => [
+                    'accountability' => $accountability,
+                ],
+            ]);
+            $isValid = false;
+        }
+
+        return $isValid;
     }
 }
