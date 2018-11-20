37752973a8cd37e2c8ddd75feb103fa0280131f7
crast <crast@users.noreply.github.com>
2018-11-19 18:52:14 -0700

    Fix issue in error messages

--- src/resources/assets/js/submission/review/components.jsx
+++ src/resources/assets/js/submission/review/components.jsx
@@ -111,7 +111,7 @@ export default class Review extends SubmissionBase {
         PAGES_CONFIG.forEach((config) => {
             const pageData = this.props[config.key]
             const rawMessages = pageData.messages
-            if (rawMessages && rawMessages.length) {
+            if (rawMessages && Object.keys(rawMessages).length) {
                 categories.push(<ReviewCategory key={config.key}
                                                 baseUri={this.baseUri()}
                                                 config={config}
