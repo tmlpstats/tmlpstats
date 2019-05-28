aff0e9b4380a6fd3244ec07fd15dbc8a6418f291
Nicholas De Souza <vision.tmlp@gmail.com>
2019-05-28 18:35:22 -0400

    Add Turtle, Close but no cigar reports

--- src/resources/assets/js/reports/global_report/pages-quarteroverview.jsx
+++ src/resources/assets/js/reports/global_report/pages-quarteroverview.jsx
@@ -3,10 +3,21 @@ import PropTypes from 'prop-types'
 
 import Scoreboard from '../../reusable/scoreboard'
 
+const VIEWS = {
+    OVERVIEW: 'Overview',
+    TURTLE: 'Turtle',
+    CLOSE: 'Close'
+}
+
 export class QuarterOverviewReport extends PureComponent {
     static propTypes = {
         initialData: PropTypes.object,
     }
+
+    state = {
+        view: VIEWS.OVERVIEW
+    }
+
     renderHeaders() {
         return (
             <thead>
@@ -17,7 +28,7 @@ export class QuarterOverviewReport extends PureComponent {
                     <th colSpan="3" className="text-center border-right border-top">Milestone 3</th>
                     <th colSpan="3" className="text-center border-right border-top">Weekend</th>
                     <th colSpan="4" className="border-right border-bottom border-top-none"></th>
-                    <th colSpan="2" className="text-center border-left border-right border-top">Historical Stats</th>
+                    <th colSpan="6" className="text-center border-left border-right border-top">Historical Stats</th>
                 </tr>
                 <tr>
                     <th className="border-right border-left">Center</th>
@@ -36,8 +47,8 @@ export class QuarterOverviewReport extends PureComponent {
                     <th className="text-center border-right">%</th>
                     <th className="text-center border-right">Points</th>
                     <th colSpan="3" className="text-center border-right">Effectiveness</th>
-                    <th className="text-center border-right">Last Qtr.</th>
-                    <th className="text-center border-right">Last Year</th>
+                    <th colSpan="3" className="text-center border-right">Last Qtr.</th>
+                    <th colSpan="3" className="text-center border-right">Last Year</th>
                 </tr>
             </thead>
         )
@@ -139,6 +150,13 @@ export class QuarterOverviewReport extends PureComponent {
             points = currentWeek.games[game].points()
         }
 
+
+        let lastQuarterIncrease = currentWeek.games[game].actual - lastQuarter.games[game].actual,
+            lastQuarterIncreasePercentage = Math.round(((currentWeek.games[game].actual - lastQuarter.games[game].actual) / (lastQuarter.games[game].actual === 0 ? 1 : lastQuarter.games[game].actual)) * 100),
+            lastYearIncrease = currentWeek.games[game].actual - lastYear.games[game].actual,
+            lastYearIncreasePercentage = Math.round(((currentWeek.games[game].actual - lastYear.games[game].actual) / (lastYear.games[game].actual === 0 ? 1 : lastYear.games[game].actual)) * 100)
+
+
         return (
             <tr key={centerName+' '+game} className={rowClass+' '+bgColorClass}>
                 <th key="center" className={bgColorClass+' border-right border-left'}>{centerDisplayName}</th>
@@ -146,8 +164,14 @@ export class QuarterOverviewReport extends PureComponent {
                 {msCols}
                 <td key="points" className={bgColorClass+' text-center border-right'}>{points}</td>
                 {effectivenessData}
+
                 <td key="lastQuarter" className={bgColorClass+' text-center border-right'}>{lastQuarter.games[game].actual}</td>
+                <td key="lastQuarterIncrease" className={bgColorClass+' text-center border-right'}>{lastQuarterIncrease <= 0 ? '' : '+'}{lastQuarterIncrease}</td>
+                <td key="lastQuarterPercentage" className={bgColorClass+' text-center border-right'}>{lastQuarterIncreasePercentage <= 0 ? '' : '+'}{lastQuarterIncreasePercentage}%</td>
+
                 <td key="lastYear" className={bgColorClass+' text-center border-right'}>{lastYear.games[game].actual}</td>
+                <td key="lastYearIncrease" className={bgColorClass+' text-center border-right'}>{lastYearIncrease <= 0 ? '' : '+'}{lastYearIncrease}</td>
+                <td key="lastYearPercentage" className={bgColorClass+' text-center border-right'}>{lastYearIncreasePercentage <= 0 ? '' : '+'}{lastYearIncreasePercentage}%</td>
             </tr>
         )
     }
@@ -173,6 +197,122 @@ export class QuarterOverviewReport extends PureComponent {
         return stats
     }
 
+    changeView = (nextView) => {
+        this.setState({ view: nextView })
+    }
+
+    renderTurtle = (reportData) => {
+        let centerData = []
+        let count = 0
+        Object.keys(reportData).forEach((center) => {
+            let m3 = reportData[center]['milestone3']
+            let final = reportData[center]['final']
+            let nextCenter = true
+            Object.keys(final.actual).forEach((game) => {
+                if(final.actual[game] - m3.actual[game] >= -3 && final.actual[game] - m3.actual[game] <= 3) {
+                    let rowClass, centerDisplayName
+                    if (nextCenter) {
+                        rowClass = 'border-top'
+                        centerDisplayName = center
+                    }
+
+                    if (center === 'Total') {
+                        rowClass = 'border-bottom'
+                    }
+                    let bgColorClass = ''
+                    if (count % 2 === 0) {
+                        bgColorClass = 'info-liter'
+                    }
+
+                    centerData.push(
+                        <tr key={center+' '+game} className={rowClass+' '+bgColorClass}>
+                            <th key="center" className={bgColorClass+' border-right border-left'}>{centerDisplayName}</th>
+                            <th key="game" className={bgColorClass+' border-right'}>{game.toUpperCase()}</th>
+                            <td key="promise" className={bgColorClass+' text-center'}>{final.promise[game]}</td>
+                            <td key="from" className={bgColorClass+' text-center'}>{m3.actual[game]}</td>
+                            <td key="to" className={bgColorClass+' text-center'}>{final.actual[game]}</td>
+                            <td key="change" className={bgColorClass+' text-center'}>{final.actual[game] - m3.actual[game]}</td>
+
+                        </tr>
+                    )
+                    nextCenter = false;
+                }
+            })
+
+            count++
+
+        })
+        return (
+            <table className="table table-condensed table-hover region-container">
+                <thead>
+                    <tr>
+                        <th colSpan="1" className="text-center border-right border-left border-top">Team</th>
+                        <th colSpan="1" className="text-center border-right border-top">Game</th>
+                        <th colSpan="1" className="text-center border-right border-top">Promise</th>
+                        <th colSpan="1" className="text-center border-right border-top">From</th>
+                        <th colSpan="1" className="text-center border-right border-top">To</th>
+                        <th colSpan="1" className="text-center border-right border-top">Change</th>
+                    </tr>
+                </thead>
+                <tbody>{centerData}</tbody>
+            </table>
+        )
+    }
+
+    renderClose = (reportData) => {
+        let centerData = []
+        let count = 0
+        Object.keys(reportData).forEach((center) => {
+            let final = reportData[center]['final']
+            let nextCenter = true
+            Object.keys(final.actual).forEach((game) => {
+                if( final.actual[game] - final.promise[game] >= -3 && final.actual[game] - final.promise[game] < 0 ) {
+                    let rowClass, centerDisplayName
+                    if (nextCenter) {
+                        rowClass = 'border-top'
+                        centerDisplayName = center
+                    }
+
+                    if (center === 'Total') {
+                        rowClass = 'border-bottom'
+                    }
+                    let bgColorClass = ''
+                    if (count % 2 === 0) {
+                        bgColorClass = 'info-liter'
+                    }
+
+                    centerData.push(
+                        <tr key={center+' '+game} className={rowClass+' '+bgColorClass}>
+                            <th key="center" className={bgColorClass+' border-right border-left'}>{centerDisplayName}</th>
+                            <th key="game" className={bgColorClass+' border-right'}>{game.toUpperCase()}</th>
+                            <td key="promise" className={bgColorClass+' text-center'}>{final.promise[game]}</td>
+                            <td key="actual" className={bgColorClass+' text-center'}>{final.actual[game]}</td>
+                            <td key="difference" className={bgColorClass+' text-center'}>{final.actual[game] - final.promise[game]}</td>
+                        </tr>
+                    )
+                    nextCenter = false
+                }
+            })
+
+            count++
+
+        })
+        return (
+            <table className="table table-condensed table-hover region-container">
+                <thead>
+                <tr>
+                    <th colSpan="1" className="text-center border-right border-left border-top">Team</th>
+                    <th colSpan="1" className="text-center border-right border-top">Game</th>
+                    <th colSpan="1" className="text-center border-right border-top">Promise</th>
+                    <th colSpan="1" className="text-center border-right border-top">Actual</th>
+                    <th colSpan="1" className="text-center border-right border-top">Difference</th>
+                </tr>
+                </thead>
+                <tbody>{centerData}</tbody>
+            </table>
+        )
+    }
+
     render() {
         const { initialData: { reportData } } = this.props
 
@@ -181,6 +321,8 @@ export class QuarterOverviewReport extends PureComponent {
         }
 
         let regionsData = []
+        let turtleData = []
+        let closeData = []
         Object.keys(reportData).forEach((region) => {
             let title
             switch (region) {
@@ -201,12 +343,41 @@ export class QuarterOverviewReport extends PureComponent {
                     </table>
                 </div>
             )
+
+            turtleData.push(
+                <div key={region} className="regionContainer">
+                    <h3>{title}</h3>
+                    {
+                        this.renderTurtle(reportData[region])
+                    }
+                </div>
+            )
+
+            closeData.push(
+                <div key={region} className="regionContainer">
+                    <h3>{title}</h3>
+                    {
+                        this.renderClose(reportData[region])
+                    }
+                </div>
+            )
         })
+
+        let report = regionsData
+        if (this.state.view === VIEWS.CLOSE) report = closeData
+        else if (this.state.view === VIEWS.TURTLE) report = turtleData
 
         return (
             <div className="table-responsive quarter-overview">
                 <p className="print-hide report-description">To print, use Google Chrome with scaling set to 45% and Background graphics checked.</p>
-                {regionsData}
+                <div className="btn-group grouping report-nav-level1" role="group">
+                    <button type="button" className="btn btn-default" onClick={() => this.changeView(VIEWS.OVERVIEW)}>Overview</button>
+                    <button type="button" className="btn btn-default" onClick={() => this.changeView(VIEWS.TURTLE)}>Turtle</button>
+                    <button type="button" className="btn btn-default" onClick={() => this.changeView(VIEWS.CLOSE)}>Close But No Cigar</button>
+                </div>
+                {
+                    report
+                }
             </div>
         )
     }
