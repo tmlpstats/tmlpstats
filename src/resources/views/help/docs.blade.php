a82d7a46251ffc7557f47cde9c92d79a689cecc6
Nicholas De Souza <vision.tmlp@gmail.com>
2020-03-15 21:11:45 -0400

    add stats docs

--- src/resources/views/help/docs.blade.php
+++ src/resources/views/help/docs.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <h1>Documentation</h1>
+
     <div>
         <h3>Points Breakdown</h3>
         <table class="table-bordered table-condensed text-center">
@@ -58,5 +59,15 @@
                 <td>Under 9</td>
             </tr>
         </table>
+    </div>
+
+    <div>
+        <h3>Local Statistician Accountability</h3>
+        <iframe src="https://docs.google.com/document/d/e/2PACX-1vSLqXo2rOTd9sKA3XaueowKCaeHXbeuBXul52q-aFbzXH52062RNC_-84_NnViURdMfeBEy7VZZ4svp/pub?embedded=true"></iframe>
+    </div>
+
+    <div>
+        <h3>Regional Statistician Accountability</h3>
+        <iframe src="https://docs.google.com/document/d/e/2PACX-1vS-PhGAnFiMXJNG1_v8c2eRuMIQapJOUWE_sZlCJs8a0E09Z_VjgAs7yaNw828m9TI9dvT-IRP1ad3Y/pub?embedded=true"></iframe>
     </div>
 @endsection
