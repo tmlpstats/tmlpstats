799cabe6fe874424ca019bde70af05d3254e1854
Nicholas De Souza <vision.tmlp@gmail.com>
2020-11-25 22:18:22 -0500

    Add Team Classrooms
    + add style

--- src/resources/views/globalreports/details/classrooms.blade.php
+++ src/resources/views/globalreports/details/classrooms.blade.php
@@ -1,18 +1,21 @@
-<div>
-    <table class="table table-condensed table-striped table-hover">
+<div style="margin-top: 10px;">
+
+    <h1>Classrooms</h1>
+
+    <table style="max-width: 300px" class="table table-condensed table-striped table-hover">
         <thead>
         <tr>
-            <th class="border-left border-right border-top">Classroom</th>
-            <th class="data-point border-right border-top"># of Participants</th>
+            <th class="border-left border-right border-top">Name</th>
+            <th class="data-point border-right border-top"># Participants</th>
         </tr>
         </thead>
         <tbody>
         @foreach ($reportData as $classroom => $numParticipants)
             <tr>
-                <td class="border-left border-right">
+                <td class="border-left border-right border-bottom">
                     {{ $classroom }}
                 </td>
-                <td class="data-point">{{ $numParticipants }}</td>
+                <td class="data-point border-right border-bottom">{{ $numParticipants }}</td>
             </tr>
         @endforeach
         </tbody>
