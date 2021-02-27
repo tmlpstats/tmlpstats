d01e5f49e3b74c482ac2489c6d2b732e4950ee9d
Nicholas De Souza <vision.tmlp@gmail.com>
2021-02-27 16:06:07 -0500

    Add Interest Form For Weekend
    + add response email

--- src/resources/views/emails/interestform/response.blade.php
+++ src/resources/views/emails/interestform/response.blade.php
@@ -4,11 +4,14 @@ Hi {{ $interest_form->firstname }},
 We are excited to hear that you are interested in the Vision Team!!!
 <br>
 <br>
-Whats Next:
+What's next for you:
 <ul>
     <li>Book your <a href="https://calendly.com/visiontmlp/intro">Vision Team Intro Call</a></li>
-    <li>Join our slack community <b><a href="https://join.slack.com/t/vision-tmlp/shared_invite/zt-n5zbp2w1-7~7LpPIPQKu50qg1aB7DwA">#VisionTeam</a></b></li>
+    <li>Join our Slack community <b><a href="https://join.slack.com/t/vision-tmlp/shared_invite/zt-n5zbp2w1-7~7LpPIPQKu50qg1aB7DwA">#VisionTeam</a></b></li>
 </ul>
+<br>
+<br>
+If you need to contact us before our Intro Call, email visiontmlp@googlegroups.com and our team will be happy to support you.
 <br>
 <br>
 Thank you again for your interest and we look forward to building with you!
