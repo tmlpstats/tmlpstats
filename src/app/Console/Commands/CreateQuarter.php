e8c8edc12aaa7a5f32b5c33ad75d4dab6987ffc3
Nicholas De Souza <vision.tmlp@gmail.com>
2021-09-25 15:28:53 -0400

    Add Schedule Job to Create Quarter
    + Add new quarter command
    + Add scheduler method

---
+++ src/app/Console/Commands/CreateQuarter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace TmlpStats\Console\Commands;
+
+use Illuminate\Console\Command;
+use DB;
+
+class CreateQuarter extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:quarter';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create the next quarter in the database and send an email to the regional statistician to set the dates.';
+
+    const QUARTER_DISTINCTIONS = [
+            'Relatedness' => ['t1' => 'Relatedness', 't2' => 'Generosity', 'next_qrt' => 'Possibility'],
+            'Possibility' => ['t1' => 'Possibility', 't2' => 'Integrity', 'next_qrt' => 'Opportunity'],
+            'Opportunity' => ['t1' => 'Opportunity', 't2' => 'Listening', 'next_qrt' => 'Action'],
+            'Action' => ['t1' => 'Action', 't2' => 'Responsibility', 'next_qrt' => 'Completion'],
+            'Completion' => ['t1' => 'Completion', 't2' => 'Completion', 'next_qrt' => 'Relatedness']
+        ];
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $last_qrt = DB::table('quarters')->orderby('created_at', 'desc')->first();
+
+        $last_qrt_distinctions = self::QUARTER_DISTINCTIONS[$last_qrt->t1_distinction];
+        $new_qrt_distinctions = self::QUARTER_DISTINCTIONS[$last_qrt_distinctions['next_qrt']];
+
+        $t1_distinction = $new_qrt_distinctions['t1'];
+        $t2_distinction = $new_qrt_distinctions['t2'];
+        $qrt_number = (($last_qrt->quarter_number) % 4) + 1;
+        $year = $qrt_number == 1 ? $last_qrt->year + 1 : $last_qrt->year;
+        $created_at = $updated_at = new \DateTime();
+
+        $id = DB::table('quarters')->insertGetId(
+            [
+                't1_distinction' => $t1_distinction,
+                't2_distinction' => $t2_distinction,
+                'quarter_number' => $qrt_number,
+                'year' => $year,
+                'created_at' => $created_at,
+                'updated_at' => $updated_at,
+            ]
+        );
+
+        $this->info(`Created a new quarter - id:${id}, t1:${t1_distinction}, t2:${t2_distinction}, qrt_num:${qrt_number} $year:{year}`);
+
+    }
+
+}
--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -1,8 +1,11 @@
 <?php
 namespace TmlpStats\Console;
 
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use TmlpStats\Quarter;
+use TmlpStats\Region;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +28,7 @@ class Kernel extends ConsoleKernel
         Commands\SendTestEmailCommand::class,
         Commands\UpdatePromise::class,
         Commands\SendInterestResponseEmails::class,
+        Commands\CreateQuarter::class,
     ];
 
     /**
@@ -36,7 +40,27 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('inspire')
-                 ->hourly();
+                 ->hourly()
+                 ->appendOutputTo(storage_path('logs/inspire.log'));
+
+        $schedule->command('create:quarter')
+                 ->everyMinute()
+                 ->when(function () {
+                     $regions = Region::isGlobal()->get();
+
+                     $earliest_cl3 = Carbon::maxValue();
+                     foreach ($regions as $region) {
+                         $qrt = Quarter::current($region)->first();
+                         $cl3_date = $qrt->getClassroom3Date($region->centers[0]);
+                         $earliest_cl3 = $cl3_date < $earliest_cl3 ? $cl3_date : $earliest_cl3;
+                     }
+
+                     $dt = Carbon::now();
+
+                     return $earliest_cl3->diffInDays($dt) == 28;
+
+                 })
+                 ->appendOutputTo(storage_path('logs/create_quarter.log'));
     }
 
 }
